cc2ac1d7a81a4992a3adb4cb74916846c422af3e
operashinobi <operashinobi@gmail.com>
2025-09-10 01:55:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '7a918953-753f-4f77-aac3-dd998fe482ea';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -7195,4 +7195,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
